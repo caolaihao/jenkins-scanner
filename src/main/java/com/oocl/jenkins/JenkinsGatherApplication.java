604543431f11bb26cbd0c35e138a049e29053642
meixuesong <meixuesong@gmail.com>
2017-10-25 15:51:13 +0800

    init project

---
+++ src/main/java/com/oocl/jenkins/JenkinsGatherApplication.java
@@ -0,0 +1,13 @@
+package com.oocl.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGatherApplication.class, args);
+
+	}
+}
---
+++ src/test/java/com/oocl/jenkins/SampleTest.java
@@ -0,0 +1,12 @@
+package com.oocl.jenkins;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+    @Test
+    public void should_Name() throws Exception {
+        assertTrue(true);
+    }
+}
